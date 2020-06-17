f62c41d9be800d2334b6cdcd1beba5c1c3625e0e
Enio Ribeiro <eniojacui12@gmail.com>
2020-06-17 19:10:56 -0300

    done

---
+++ src/pages/Orders/index.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { Image } from 'react-native';
+
+import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  FoodsContainer,
+  FoodList,
+  Food,
+  FoodImageContainer,
+  FoodContent,
+  FoodTitle,
+  FoodDescription,
+  FoodPricing,
+} from './styles';
+
+interface Food {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  formattedPrice: number;
+  thumbnail_url: string;
+}
+
+const Orders: React.FC = () => {
+  const [orders, setOrders] = useState<Food[]>([]);
+
+  useEffect(() => {
+    async function loadOrders(): Promise<void> {
+      api.get('/orders').then(({ data: orderList }) => {
+        setOrders(orderList);
+      });
+    }
+
+    loadOrders();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>Meus pedidos</HeaderTitle>
+      </Header>
+
+      <FoodsContainer>
+        <FoodList
+          data={orders}
+          keyExtractor={(item: Food) => String(item.id)}
+          renderItem={({ item }: { item: Food }) => (
+            <Food key={item.id} activeOpacity={0.6}>
+              <FoodImageContainer>
+                <Image
+                  style={{ width: 88, height: 88 }}
+                  source={{ uri: item.thumbnail_url }}
+                />
+              </FoodImageContainer>
+              <FoodContent>
+                <FoodTitle>{item.name}</FoodTitle>
+                <FoodDescription>{item.description}</FoodDescription>
+                <FoodPricing>{item.formattedPrice}</FoodPricing>
+              </FoodContent>
+            </Food>
+          )}
+        />
+      </FoodsContainer>
+    </Container>
+  );
+};
+
+export default Orders;
